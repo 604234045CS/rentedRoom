1851647df484f268b5909472f8b70a3907f317d7
604234045CS <604234045@parichat.skru.ac.th>
2019-11-12 14:38:35 +0700

    really

--- src/pages/insert-dorm/insert-dorm.ts
+++ src/pages/insert-dorm/insert-dorm.ts
@@ -15,12 +15,12 @@ export class InsertDormPage {
   constructor(public navCtrl: NavController, public navParams: NavParams,public http:Http,public alertCtrl : AlertController) {
    
     this.insertdata.dorm_name = "";
-    this.insertdata.address = "";
     this.insertdata.price = "";
+    this.insertdata.catagory = "";
     this.insertdata.sex_c = "";
     this.insertdata.numphone = "";
-    this.insertdata.catagory = "";
     this.insertdata.facilities = "";
+    this.insertdata.address = "";
   }
 
   ionViewDidLoad() {
@@ -32,22 +32,22 @@ export class InsertDormPage {
     && this.insertdata.price != ""&& this.insertdata.sex_c != "" && this.insertdata.numphone != "" 
     && this.insertdata.catagory != "" && this.insertdata.facilities != ""){
       console.log("dorm_name:",this.insertdata.dorm_name);
-      console.log("address:",this.insertdata.address);
-      console.log("price:",this.insertdata.price);
+      console.log("address:",this.insertdata.price);
+      console.log("price:",this.insertdata.catagory);
       console.log("sex_c:",this.insertdata.sex_c);
       console.log("numphone:",this.insertdata.numphone);
-      console.log("catagory:",this.insertdata.catagory);
-      console.log("facilities:",this.insertdata.facilities);
+      console.log("catagory:",this.insertdata.facilities);
+      console.log("facilities:",this.insertdata.address);
 
       let url = 'http://10.8.8.198/ranted/addroom.php';
       let dataPost = JSON.stringify({
         dorm_name: this.insertdata.dorm_name,
-        address: this.insertdata.address,
-        price: this.insertdata.price,
+        address: this.insertdata.price,
+        price: this.insertdata.catagory,
         sex_c: this.insertdata.sex_c,
         numphone: this.insertdata.numphone,
-        catagory: this.insertdata.catagory,
-        facilities: this.insertdata.facilities
+        catagory: this.insertdata.facilities,
+        facilities: this.insertdata.address
       });
       this.http.post(url,dataPost)
       .subscribe(data =>{
